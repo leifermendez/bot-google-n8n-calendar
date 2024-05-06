637de3409ea822965a1ee200db727bdb4fba93c7
Leifer Mendez <leifer33@gmail.com>
2024-05-06 18:59:15 +0200

    fix: provider bayle

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,8 +1,4 @@
 import { createProvider } from '@builderbot/bot'
-import { WaliProvider } from '@builderbot-plugins/wali'
+import { BaileysProvider } from '@builderbot/provider-baileys'
 
-export const provider = createProvider(WaliProvider,{
-    deviceId:process.env.CONSOLE_DEVICE_ID,
-    token:process.env.CONSOLE_KEY,
-    api:'https://wa-api.builderbot.app'
-})
+export const provider = createProvider(BaileysProvider)
