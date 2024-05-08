597ad1ec46318f916db9b216fe0feea195628336
Leifer Mendez <leifer33@gmail.com>
2024-05-08 13:27:50 +0200

    Delete src/provider/telegram.ts

--- src/provider/telegram.ts
+++
@@ -1,6 +0,0 @@
-import { createProvider } from "@builderbot/bot";
-import { TelegramProvider } from '@builderbot-plugins/telegram'
-
-export const providerTelegram = createProvider(TelegramProvider, {
-    token: process.env.TELEGRAM_API ?? ''
-})
