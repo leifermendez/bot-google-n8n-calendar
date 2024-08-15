eda838240779dadc9491ebca9094bbe34dbc770a
Leifer Mendez <leifer33@gmail.com>
2024-08-15 12:35:55 +0200

    fix: update

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,29 @@ import AIClass from './services/ai';
 import flow from './flows';
 import { provider } from './provider';
 
+/** Puerto en el que se ejecutará el servidor */
 const PORT = process.env.PORT ?? 3001
-const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo-0125')
+/** Instancia de la clase AI */
+const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-4o-mini')
 
+/**
+ * Función principal que inicializa y configura el bot
+ * @async
+ */
 const main = async () => {
+    /** Objeto que contiene el servidor HTTP y el manejador de contexto */
     const { httpServer, handleCtx } = await createBot({
         database: new MemoryDB(),
         provider,
         flow,
     }, { extensions: { ai } })
 
-
+    /**
+     * Ruta POST para enviar mensajes
+     * @param {string} number - Número de teléfono del destinatario
+     * @param {string} message - Mensaje a enviar
+     * @param {string} [urlMedia] - URL del archivo multimedia (opcional)
+     */
     provider.server.post(
         '/v1/messages',
         handleCtx(async (bot, req, res) => {
@@ -24,6 +36,11 @@ const main = async () => {
         })
     )
 
+    /**
+     * Ruta POST para registrar un nuevo usuario
+     * @param {string} number - Número de teléfono del usuario
+     * @param {string} name - Nombre del usuario
+     */
     provider.server.post(
         '/v1/register',
         handleCtx(async (bot, req, res) => {
@@ -33,7 +50,11 @@ const main = async () => {
         })
     )
 
-
+    /**
+     * Ruta POST para gestionar la lista negra
+     * @param {string} number - Número de teléfono a añadir o eliminar de la lista negra
+     * @param {('add'|'remove')} intent - Acción a realizar (añadir o eliminar)
+     */
     provider.server.post(
         '/v1/blacklist',
         handleCtx(async (bot, req, res) => {
@@ -46,6 +67,9 @@ const main = async () => {
         })
     )
 
+    /** Inicia el servidor HTTP en el puerto especificado */
     httpServer(+PORT)
 }
+
+// Ejecuta la función principal
 main()
--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,4 +1,7 @@
 import { createProvider } from '@builderbot/bot'
 import { BaileysProvider } from '@builderbot/provider-baileys'
 
-export const provider = createProvider(BaileysProvider)
+export const provider = createProvider(BaileysProvider, {
+    groupsIgnore: true,
+    readStatus: false
+})
